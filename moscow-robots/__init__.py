cc319759d53a90922c4436cf81f2280555f4edb7
Milya Rayko <rayko@niisi.ru>
2024-03-05 21:42:40 +0300

    Creating package structure

---
+++ moscow-robots/__init__.py
@@ -0,0 +1,9 @@
+from .data import get_image
+from .piktomir import Game
+
+__version__ = "0.0.1"
+__author__ = "Dmitrij Preobrazhenskij, Alexander A. Maly, Milya Rayko"
+__copyright__ = "Copyright 2024"
+__maintainer__ = "Milya Rayko"
+__license__ = "GPL"
+__status__ = "early development"
