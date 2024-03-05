ccb5174011fe830fe2110ab19fa47fe6b7d7145e
Alexander A. Maly <a.a.maly@gmail.com>
2024-03-05 21:42:40 +0300

    Creating package structure

---
+++ moscow-robots/__init__.py
@@ -0,0 +1,9 @@
+from .data import get_image
+from .piktomir import Game
+
+__version__ = "0.0.1"
+__author__ = "Dmitrij Preobrazhenskij, Alexander A. Maly"
+__copyright__ = "Copyright 2024"
+__maintainer__ = "Alexander A. Maly"
+__license__ = "GPL"
+__status__ = "early development"
