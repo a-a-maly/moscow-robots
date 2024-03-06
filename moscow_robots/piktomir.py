32fc81a858e8a59e22f1ac580c161d06a6fd05b8
Alexander A. Maly <a.a.maly@gmail.com>
2024-03-06 21:47:24 +0300

    Clean textures
    from robots.py and piktomir.py

--- moscow_robots/piktomir.py
+++ moscow_robots/piktomir.py
@@ -2,6 +2,7 @@ import pygame
 import time
 import json
 from moscow_robots import robots as function_robots
+from moscow_robots.data import get_image
 
 
 # 2.1.4
@@ -67,18 +68,10 @@ class Pole:
         self.screen = screen
         function_robots.first_field_textures(self)
 
-        self.warning_message = pygame.image.load(self.textures["warning"]).convert_alpha()
+        self.warning_message = get_image("msg_instruction").convert_alpha()
         self.warning_message = pygame.transform.scale(self.warning_message,
                                                       (3 * self.screen_resolution[0] / 5,
                                                        self.screen_resolution[0] / 5))
-
-        if robot in [1, 2, 5]:
-            self.block_surface = pygame.image.load(textures["block"]).convert()
-            self.block_surface = pygame.transform.scale(self.block_surface,
-                                                        (self.cell_size[0] * 0.8, self.cell_size[1] * 0.8))
-            self.block2_surface = pygame.image.load(textures["block2"]).convert_alpha()
-            self.block2_surface = pygame.transform.scale(self.block2_surface,
-                                                         (self.cell_size[0] * 0.8, self.cell_size[1] * 0.8))
 
         self.robot = robot
 
@@ -101,73 +94,7 @@ class Game:
         with open(name_json, "r") as json_file:
             initial_data = json.load(json_file)
         self.array_command = []
-        self.textures = {
-            "vertun_surface": "textures/Vertun_2D.png",
-            "broken_vertun_surface": "textures/Vertun_2D_obstacle.png",
-
-            "dvigun_surface": "textures/prototype2.png",
-            "broken_dvigun_surface": "textures/Vertun_2D_obstacle.png",
-            "tyagun_surface": "textures/tyagun.png",
-
-            "polzun_surface": "textures/polzun_surface.png",
-            "polzun_loaded": "textures/polzun_loaded.png",
-            "broken_polzun": "textures/polzun_obstacle.png",
-            "polzun_loaded_dead": "textures/tolkun_loaded_dead.png",
-
-            "tolkun_surface": "textures/tolkun_surface.png",
-            "tolkun_loaded": "textures/tolkun_loaded.png",
-            "broken_tolkun": "textures/tolkun_obstacle.png",
-            "tolkun_loaded_dead": "textures/tolkun_loaded_dead.png",
-
-            "train_surface": "textures/train_surface.png",
-            "broken_train_surface": "textures/train_obstacle.png",
-            "scepka": "textures/scepka.png",
-
-            "broken_cell_surface": "textures/mapElement_2_2d.png",
-            "painted_broken_cell_surface": "textures/mapElement_4_2d.png",
-            "painted_cell_surface": "textures/mapElement_3_2d.png",
-            "usual_cell_surface": "textures/mapElement_1_2d.png",
-            "usual_cell_surface2": "textures/mapElement_5_2d.png",
-            "usual_cell_surface3": "textures/mapElement_6_2d.png",
-            "_wall_surface": "textures/wall-top_view.png",
-            "Iwall_surface": "textures/wall-top_view2.png",
-            "end_polzun": "textures/mapElementR.png",
-            "end_place": "textures/cross.png",
-            "finish_block": 'textures/finish_block.png',
-            "finish_block2": 'textures/finish_block2.png',
-            "finish_blocku": 'textures/finish_block_u.png',
-            "block": "textures/block.png",
-            "block2": "textures/block2.png",
-            "rblock0": "textures/rblock0.png",
-            "rblock1": "textures/rblock1.png",
-            "rblock2": "textures/rblock2.png",
-            "rblock3": "textures/rblock3.png",
-            "rblock4": "textures/rblock4.png",
-            "rblock5": "textures/rblock5.png",
-            "rblock6": "textures/rblock6.png",
-            "rblock7": "textures/rblock7.png",
-            "rblock8": "textures/rblock8.png",
-            "rblock9": "textures/rblock9.png",
-            "warning": "textures/warning.png",
-            "win": "textures/win.png",
-            "lose": "textures/lose.png",
-            "standart_cell_polzun": "textures/mapElement_6_2d.png",
-            "division": "textures/division.png",
-            "equality": "textures/equality.png",
-            "minus": "textures/minus.png",
-            "multiply": "textures/multiplication.png",
-            "num0": "textures/num0.png",
-            "num1": "textures/num1.png",
-            "num2": "textures/num2.png",
-            "num3": "textures/num3.png",
-            "num4": "textures/num4.png",
-            "num5": "textures/num5.png",
-            "num6": "textures/num6.png",
-            "num7": "textures/num7.png",
-            "num8": "textures/num8.png",
-            "num9": "textures/num9.png",
-            "plus": "textures/plus.png"
-        }
+        self.textures = {}
         self.name_json = name_json
         self.speed = 550
         settings = initial_data["settings"]
@@ -186,10 +113,10 @@ class Game:
             self.num_robot = settings["robot"]
             print("num_robot:", str(self.num_robot))
             pygame.init()
-            if "textures" in initial_data.keys():
-                for texture in self.textures.keys():
-                    if texture in initial_data["textures"].keys():
-                        self.textures[texture] = initial_data["textures"][texture]
+#            if "textures" in initial_data.keys():
+#                for texture in self.textures.keys():
+#                    if texture in initial_data["textures"].keys():
+#                        self.textures[texture] = initial_data["textures"][texture]
             self.screen = pygame.display.set_mode(settings["screen_resolution"])
             self.vertical_fences = []
             if "vertical_fences" in initial_data.keys():
@@ -224,10 +151,10 @@ class Game:
                 self.robot = function_robots.Tolkun(self)
             elif self.num_robot == 5:
                 self.robot = function_robots.Train(self)
-            self.win_message = pygame.image.load(self.textures["win"]).convert_alpha()
+            self.win_message = get_image("msg_win").convert_alpha()
             self.win_message = pygame.transform.scale(self.win_message, (3 * self.pole.screen_resolution[0] / 5,
                                                                          1.5 * self.pole.screen_resolution[1] / 5))
-            self.lose_message = pygame.image.load(self.textures["lose"]).convert_alpha()
+            self.lose_message = get_image("msg_lose").convert_alpha()
             self.lose_message = pygame.transform.scale(self.lose_message, (3 * self.pole.screen_resolution[0] / 5,
                                                                            1.5 * self.pole.screen_resolution[1] / 5))
 
@@ -278,7 +205,6 @@ class Game:
         if self.check == 0:
             self.pole.make_space()
             function_robots.make_robot(self.robot)
-            # self.robot.make_vertun()
             self.pole.warning()
             pygame.display.update()
             pygame.time.delay(500)
