328f7c790c319107036f05fbef214faa0ac4262e
Milya Rayko <rayko@niisi.ru>
2024-03-11 22:26:10 +0300

    Added Vertun example

---
+++ examples/vertun/vertun-001_game.py
@@ -0,0 +1,3 @@
+from moscow_robots import Game
+mg = Game("vertun-001.json")
+mg.main_loop()
---
+++ examples/vertun/vertun_edit.py
@@ -0,0 +1,14 @@
+import moscow_robots as mr
+
+settings = {
+	"max_x" : 8,
+	"max_y" : 8,
+	"screen_resolution": (800,800),
+	"start_x" : 0,
+	"start_y" : 0,
+	"robot" : 0,
+	"angle" : 0,
+}
+
+mr.edit_level("vertun-001.json")
+
