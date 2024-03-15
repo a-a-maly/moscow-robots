e5b4d3b2fe9905f94fcae203cc9c8ed11973dfe6
Alexander A. Maly <a.a.maly@gmail.com>
2024-03-15 15:17:25 +0300

    New format support updated

--- moscow_robots/__init__.py
+++ moscow_robots/__init__.py
@@ -1,8 +1,9 @@
 from .data import get_image
 from .piktomir import Game
 from .editor import edit_level
+from .game import GameRobot
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __author__ = "Dmitrij Preobrazhenskij, Alexander A. Maly"
 __copyright__ = "Copyright 2024"
 __maintainer__ = "Alexander A. Maly"
