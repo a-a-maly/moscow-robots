c62ee2f417a62a5180016b185d815aa45a0f8dfc
Alexander A. Maly <a.a.maly@gmail.com>
2024-03-15 22:05:49 +0300

    Fixing new variant game

--- moscow_robots/game.py
+++ moscow_robots/game.py
@@ -1,6 +1,7 @@
 import pygame
 import json
 from moscow_robots.data import get_image
+import sys
 
 
 class Robot:
@@ -87,8 +88,12 @@ class Textures:
 
 
 class GameRobot:
+
     def __init__(self, json_name):
-        self.base_name = json_name.split(".")[:-1]
+        self.direction_vectors = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+
+        self.base_name = "".join(json_name.split(".")[:-1])
+        print("base_name", self.base_name)
         with open(json_name, "r") as json_file:
             self.json_data = json.load(json_file)
 
@@ -111,7 +116,7 @@ class GameRobot:
         self.screen = pygame.display.set_mode(self.ssize)
         self.textures = Textures(self.robot_kind, self.csize)
 
-        self.speed = 500
+        self.speed = 100
         self.game_mode = 0
         self.robot_alive = True
         self.background = None
@@ -119,24 +124,16 @@ class GameRobot:
 
     def __enter__(self):
         self.robot_alive = True
-        self.game_mode = 0
-        self.redraw_field()
-        self.redraw_robot()
-        #self.screen.blit(self.textures.msg_intro, self.csize)
-        pygame.display.update() 
-  
-        while self.game_mode == 0:
-            for ev in pygame.event.get():
-                if ev.type == pygame.KEYDOWN:
-                    if ev.key == pygame.K_1:
-                        self.game_mode = 1
-                    elif ev.key == pygame.K_2:
-                        self.game_mode = 2
-                    elif ev.key == pygame.K_ESCAPE:
-                        self.game_mode = 3
+        self.game_mode = 2 # step by step
+        self.finish_step(True, False)
+        return self
 
     def __exit__(self, t, v, tb):
-        self.game_mode = 4
+        print(t)
+        print(v)
+        print(tb)
+        if v == "Robot is dead":
+            return True
 
     def redraw_field(self):
         self.screen.fill((255, 0, 255)) 
@@ -195,3 +192,137 @@ class GameRobot:
         by = cy * y + cy // 10
         self.screen.blit(t, (bx, by))
 
+    def finish_step(self, need_redraw=True, need_wait=True):
+        if need_redraw or not self.robot_alive:
+            self.redraw_field()
+            self.redraw_robot()
+        pygame.display.update()
+
+        if need_wait:
+            pygame.time.wait(self.speed)
+
+        flag = (self.game_mode == 2)
+        while True:
+            for ev in pygame.event.get():
+                if ev.type == pygame.QUIT:
+                    sys.exit("Closed by user ")
+                if ev.type == pygame.KEYDOWN:
+                    if ev.key == pygame.K_TAB:
+                        flag = False
+                        self.game_mode = 1
+                    elif ev.key == pygame.K_SPACE:
+                        flag = False
+                        self.game_mode = 2
+            if not flag:
+                break            
+        if not self.robot_alive:
+            raise RuntimeError("Robot is dead")
+
+
+    def _path_clear(self):
+        sx = self.field.sx
+        sy = self.field.sy
+        x = self.robot.x
+        y = self.robot.y
+        d = self.robot.dir % 4
+        dx, dy = self.direction_vectors[d]
+        x1 = x + dx
+        y1 = y + dy
+        if (x1 < 0) or (x1 >= sx) or (y1 < 0) or (y1 >= sy):
+            return False
+        if dx > 0 and self.field.vfences[y][x]:
+            return False
+        if dx < 0 and self.field.vfences[y][x - 1]:
+            return Flase
+        if dy > 0 and self.field.hfences[y][x]:
+            return False
+        if dy < 0 and self.field.hfences[y - 1][x]:
+            return False
+        return True
+
+    def _cell_normal(self):
+        c = self.field.cells[self.robot.y][self.robot.x]
+        return not c.broken or c.painted
+
+    def _cell_broken(self):
+        c = self.field.cells[self.robot.y][self.robot.x]
+        return c.broken and not c.painted
+
+    def _cell_fixed(self):
+        c = self.field.cells[self.robot.y][self.robot.x]
+        return c.painted
+
+    def _fix_cell(self):
+        if not self.robot_alive:
+            return False
+        self.field.cells[self.robot.y][self.robot.x].painted = 1
+        return True
+
+    def _step_forward(self):
+        if not self.robot_alive:
+            return False
+        if not self._path_clear():
+            self.robot_alive = False
+        else:
+            d = self.robot.dir % 4
+            dx, dy = self.direction_vectors[d]
+            self.robot.x += dx
+            self.robot.y += dy
+        return True
+
+    def _turn_right(self): 
+        if not self.robot_alive:
+            return False
+        d = (self.robot.dir + 1) % 4
+        self.robot.dir = d
+        return True
+        
+    def _turn_left(self): 
+        if not self.robot_alive:
+            return False
+        d = (self.robot.dir + 3) % 4
+        self.robot.dir = d
+        return True
+
+    def cell_normal(self):
+        ans = self._cell_normal()
+        self.finish_step(False, True)
+
+    def cell_broken(self):
+        ans = self._cell_broken()
+        self.finish_step(False, True)
+
+    def cell_fixed(self):
+        ans = self._cell_fixed()
+        self.finish_step(False, True)
+
+    def path_clear(self):
+        ans = self._path_clear()
+        self.finish_step(False, True)
+        return ans
+
+    def step_forward(self):
+        ans = self._step_forward()
+        self.finish_step(ans, True)
+
+    def turn_right(self): 
+        ans = self._turn_right()
+        self.finish_step(ans, True)
+
+    def turn_left(self): 
+        ans = self._turn_left()
+        self.finish_step(ans, True)
+
+    def fix_cell(self):
+        ans = self._fix_cell()
+        self.finish_step(ans, True)
+
+        
+
+
+
+
+
+
+
+ 
