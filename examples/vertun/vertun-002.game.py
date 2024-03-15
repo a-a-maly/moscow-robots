9e3b2edebd1fbb099816496e7d1c1002f2113527
Milya Rayko <rayko@niisi.ru>
2024-03-15 22:05:49 +0300

    Fixing new variant game

--- examples/vertun/vertun-002.game.py
+++ examples/vertun/vertun-002.game.py
@@ -1,5 +1,31 @@
 import moscow_robots as mr
 
 with mr.GameRobot("vertun-002.json") as  r:
-    pass
+    print(type(r))
+
+    while r.path_clear():
+        r.step_forward()
+    r.step_forward()
+    r.turn_right()
+    r.step_forward()
+    r.turn_right()
+    while r.path_clear():
+        r.step_forward()
+    r.turn_left()
+    r.turn_left()
+    while r.path_clear():
+        r.fix_cell()
+        r.step_forward()
+    r.turn_right()
+    r.step_forward()
+    r.turn_right()
+    while r.path_clear():
+        r.step_forward()
+    r.turn_left()
+    r.turn_left()
+    r.fix_cell()
+    while r.path_clear():
+        r.step_forward()
+
+    
 
