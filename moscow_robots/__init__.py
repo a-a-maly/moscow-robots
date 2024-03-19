7a9e53402371f312da820884983ac3ce62bd6b95
Milya Rayko <rayko@niisi.ru>
2024-03-19 22:24:13 +0300

    Fixed GameVertun path

--- moscow_robots/__init__.py
+++ moscow_robots/__init__.py
@@ -1,9 +1,9 @@
 from .data import get_image
 from .piktomir import Game
 from .editor import edit_level
-from .game import GameVertun
+from .game_vertun import GameVertun
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 __author__ = "Dmitrij Preobrazhenskij, Alexander A. Maly, Milya Rayko"
 __copyright__ = "Copyright 2024"
 __maintainer__ = "Milya Rayko"
