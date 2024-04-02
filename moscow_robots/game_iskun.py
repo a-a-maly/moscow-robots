4d456ce2ca64a7f0bdacdef29ed1b64ff8fe311c
Milya Rayko <rayko@niisi.ru>
2024-04-02 17:18:54 +0300

    Optimized pygame time consumption in Iskun robot

--- moscow_robots/game_iskun.py
+++ moscow_robots/game_iskun.py
@@ -61,7 +61,10 @@ class GameIskun:
         self.csize = (csize, csize)
         self.ssize = (self.csize[0] * max(3, self.fsize[0]), self.csize[1] * max(2, self.fsize[1]))    
 
-        pygame.init()
+        pygame.display.init()
+        pygame.event.set_blocked(pygame.MOUSEMOTION)
+        pygame.event.set_blocked(pygame.WINDOWMOVED)
+
         self.screen = pygame.display.set_mode(self.ssize)
         self.textures = Textures(self.robot_kind, self.csize)
 
@@ -196,10 +199,15 @@ class GameIskun:
 
         flag = ((self.game_mode == 2) or (not self.robot_alive)) and need_raise
         while True:
+            pygame.event.pump()
             for ev in pygame.event.get():
+                if ev.type == pygame.WINDOWSHOWN:
+                    pygame.display.flip()
+                    continue
                 if ev.type == pygame.QUIT:
                     sys.exit("Closed by user ")
                     flag = False
+                    continue
                 if ev.type == pygame.KEYDOWN:
                     if ev.key == pygame.K_TAB:
                         flag = False
@@ -207,8 +215,12 @@ class GameIskun:
                     elif ev.key == pygame.K_SPACE:
                         flag = False
                         self.game_mode = 2
+                    continue
+
             if not flag:
                 break
+            pygame.time.wait(10)
+
         if need_raise and not self.robot_alive:
             raise RuntimeError("Robot is dead")
 
