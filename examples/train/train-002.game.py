a81ab4b59d919dc1d2374eb5c9200c5cd0d31a47
Milya Rayko <rayko@niisi.ru>
2024-05-03 16:58:45 +0300

    Added Train robot example

---
+++ examples/train/train-002.game.py
@@ -0,0 +1,23 @@
+#!python3
+import moscow_robots as mr
+
+def prog_A(r):
+    while r.path_clear():
+        r.step_forward()
+
+def prog_B(r):
+    while r.path_clear():
+        r.tow()
+
+with mr.GameTrain("train-002.json") as  r:
+
+    for _ in range(2):
+        prog_A(r)
+        r.turn_left()
+        r.turn_left()
+        r.add()
+        prog_B(r)
+        r.turn_left()
+        r.step_forward()
+        r.turn_left()
+
